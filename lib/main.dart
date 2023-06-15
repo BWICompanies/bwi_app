5c63631f855042c46b1962fba675dd52cdf0a710
joer80 <joer80@gmail.com>
2023-06-15 12:01:32 -0500

    Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,21 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+/*
+  class _MainPageState extends State<MainPage> {
+  int currentIndex = 0; // default index of the bottom navigation bar
+
+
+  final screens = [
+    HomeScreen(),
+    CatalogScreen(),
+    ScanScreen(),
+    HistoryScreen(),
+    AccountScreen(),
+  
+  ];
+ */
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
