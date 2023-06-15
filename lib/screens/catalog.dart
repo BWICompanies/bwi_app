bb90528d4252d8789053453f65605b4a84d3926e
joer80 <joer80@gmail.com>
2023-06-15 08:15:30 -0500

    Move rest to another page

---
+++ lib/screens/catalog.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_example/model/user_model.dart';
+import 'package:rest_api_example/services/api_service.dart';
+import '../shared/shared_file.dart';
+
+class Catalog extends StatefulWidget {
+  const Catalog({Key? key}) : super(key: key);
+
+  @override
+  _CatalogState createState() => _CatalogState();
+}
+
+class _CatalogState extends State<Catalog> {
+  late List<UserModel>? _userModel = []; //declare but initialize later
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _userModel = (await ApiService().getUsers())!;
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('REST API Example'),
+      ),
+      body: _userModel == null || _userModel!.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _userModel!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].id.toString()),
+                          Text(_userModel![index].username),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].email),
+                          Text(_userModel![index].website),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rest_api_example/model/user_model.dart';
-import 'package:rest_api_example/services/api_service.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,56 +8,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late List<UserModel>? _userModel = []; //declare but initialize later
-  @override
-  void initState() {
-    super.initState();
-    _getData();
-  }
-
-  void _getData() async {
-    _userModel = (await ApiService().getUsers())!;
-    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('REST API Example'),
+        title: const Text('BWI Home'),
       ),
-      body: _userModel == null || _userModel!.isEmpty
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: _userModel!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(_userModel![index].id.toString()),
-                          Text(_userModel![index].username),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(_userModel![index].email),
-                          Text(_userModel![index].website),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+      body: const Center(
+        child: Text('Home'),
+      ),
     );
   }
 }
