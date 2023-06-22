a868c39c85b92c1fd58c3787931462948a01c5a0
joer80 <joer80@gmail.com>
2023-06-22 10:03:54 -0500

    Routing example

--- android/app/src/main/kotlin/com/example/rest_api_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rest_api_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,47 @@
+// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:rest_api_example/screens/home.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:window_size/window_size.dart';
 
-void main() => runApp(const MyApp());
+import 'src/app.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() {
+  // Use package:url_strategy until this pull request is released:
+  // https://github.com/flutter/flutter/pull/77103
 
-/*
-  class _MainPageState extends State<MainPage> {
-  int currentIndex = 0; // default index of the bottom navigation bar
+  // Use to setHashUrlStrategy() to use "/#/" in the address bar (default). Use
+  // setPathUrlStrategy() to use the path. You may need to configure your web
+  // server to redirect all paths to index.html.
+  //
+  // On mobile platforms, both functions are no-ops.
+  setHashUrlStrategy();
+  // setPathUrlStrategy();
 
+  setupWindow();
+  runApp(const Bookstore());
+}
 
-  final screens = [
-    HomeScreen(),
-    CatalogScreen(),
-    ScanScreen(),
-    HistoryScreen(),
-    AccountScreen(),
-  
-  ];
- */
+const double windowWidth = 480;
+const double windowHeight = 854;
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: Home(),
-    );
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Navigation and routing');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
   }
 }
