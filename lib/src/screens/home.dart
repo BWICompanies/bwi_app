f27c07e7e975a389e66a8043c828a5f3131e3946
joer80 <joer80@gmail.com>
2023-08-03 16:57:44 -0500

    created homepage card design

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -18,6 +18,71 @@ class HomeScreen extends StatelessWidget {
           title: Text(title),
           backgroundColor: Colors.green[700],
         ),
-        body: Text('Homepage'),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.0,
+              crossAxisSpacing: 20.0,
+              children: [
+                InkWell(
+                  //onTap: () => _handleBookTapped(context, book),
+                  child: Container(
+                    padding: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Icon(
+                            Icons.qr_code_scanner,
+                            color: Colors.green[700],
+                            size: 60,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            'Scan Barcode',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  //onTap: () => _handleBookTapped(context, book),
+                  child: Container(
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Icon(
+                            Icons.auto_stories,
+                            color: Colors.green[700],
+                            size: 60,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            'Catalog',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ]),
+        ),
       );
 }
