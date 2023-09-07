aaea9d30eb32bac0c236324ae886930329b143e0
joer80 <joer80@gmail.com>
2023-09-07 10:38:02 -0500

    Scan code WIP

--- lib/src/screens/scan.dart
+++ lib/src/screens/scan.dart
@@ -8,19 +8,75 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import '../data/library.dart';
 import '../routing.dart';
 
-String _scanBarcode = 'Unknown';
-
-class ScanScreen extends StatelessWidget {
-  final String title = 'Scan';
-
+class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  String _scanBarcode = 'Unknown';
+
+  /// For Continuous scan. (Doesnt return value, just a future function that sets a variable without blocking the interface)
+  Future<void> startBarcodeScanStream() async {
+    //This method will return a persistent stream of barcode scans until the client drops the activity. Returns a stream of barcode strings that were recognized.
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> barcodeScan() async {
+    String barcodeScanRes;
+
+    //This method will return outcome of scan as string. (Pass in scan line color, cancel button text, show flash icon, and ScanMode [QR, BARCODE, DEFAULT]
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+
+    //tmp removed try catch. Platform messages may fail, so we use a try/catch PlatformException.
+    /*
+      try {
+        barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#ff6666', 'Cancel', true, ScanMode.QR);
+        print(barcodeScanRes);
+      } on PlatformException {
+        barcodeScanRes = 'Failed to get platform version.';
+      }
+      if (!mounted) return;
+      setState(() {
+        _scanBarcode = barcodeScanRes;
+      });
+    }
+    */
+
+    print(barcodeScanRes);
+
+    _scanBarcode = barcodeScanRes;
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: const Text('Scan'),
           backgroundColor: Colors.green[700],
         ),
-        body: Text('Scan Page'),
+        body: Container(
+            alignment: Alignment.center,
+            child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Scan result : $_scanBarcode\n',
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 45,
+                    child: ElevatedButton(
+                        onPressed: () => barcodeScan(),
+                        child: const Text('Barcode Scan',
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold))),
+                  ),
+                ])),
       );
 }
