f48d05a383bce1430062d2ea3e18bbc61a872480
joer80 <joer80@gmail.com>
2023-11-01 15:57:14 -0500

    imports

---
+++ lib/src/constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static String baseUrl = 'https://api.bwicompanies.com';
+  static String usersEndpoint = '/v1/users/auth';
+}
--- lib/src/screens/history.dart
+++ lib/src/screens/history.dart
@@ -1,14 +1,8 @@
-// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-
-import '../data/library.dart';
 import '../routing.dart';
-//import 'package:http/http.dart' as http;
-//import 'package:rest_api_example/constants.dart';
-//import 'package:rest_api_example/model/user_model.dart';
+import '../constants.dart'; //ie. var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+import '../data.dart';
+import 'package:http/http.dart' as http;
 //import 'package:shared_preferences/shared_preferences.dart';
 
 class HistoryScreen extends StatelessWidget {
