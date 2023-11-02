444e9bf8bf21b590a71526b8030e9e3f9f7395dc
joer80 <joer80@gmail.com>
2023-11-02 09:20:47 -0500

    auth WIP

--- lib/src/auth.dart
+++ lib/src/auth.dart
@@ -2,12 +2,13 @@ import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import './constants.dart'; //ie. var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+import 'dart:convert';
 
 //Navigator runs .signIn when the user clicks the sign in button
 class ProductstoreAuth extends ChangeNotifier {
   //Priviate class variables
   bool _signedIn = false;
-  String _userName = "Guest";
+  //String _userName = "Guest";
   //String _userAccountNum = "12345";
   //String _activeAccount = "12345";
   //String _accountStatus = "I";
@@ -23,15 +24,7 @@ class ProductstoreAuth extends ChangeNotifier {
 
   //getter methods to allow outside access to the private class variables
   bool get signedIn => _signedIn; //ie. yourInstance.signedIn
-  String get userName => _userName; //ProductstoreAuthScope.of(context).userName
-
-  Future<void> signOut() async {
-    _signedIn = false;
-    notifyListeners();
-
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
-  }
+  //String get userName => _userName; //ProductstoreAuthScope.of(context).userName
 
   Future<bool> signIn(String email, String password) async {
     var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.authEndpoint);
@@ -47,6 +40,7 @@ class ProductstoreAuth extends ChangeNotifier {
     if (response.statusCode == 200) {
       String token = response.body;
       await saveToken(token);
+      await saveUserData(token);
       _signedIn = true;
       notifyListeners();
       return true;
@@ -59,6 +53,15 @@ class ProductstoreAuth extends ChangeNotifier {
 
     return false;
   }
+  //end sign in
+
+  Future<void> signOut() async {
+    _signedIn = false;
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
 
   saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
@@ -70,6 +73,46 @@ class ProductstoreAuth extends ChangeNotifier {
     return prefs.getString('token');
   }
 
+  saveUserData(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+
+    final response = await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      //List<dynamic> data = json.decode(response.body);
+
+      // Parse the JSON string
+      Map<String, dynamic> jsonData = json.decode(response.body);
+
+      // Access the "accountnum" value
+      String accountNum = jsonData['data']['accountnum'];
+
+      await prefs.setString('accountnum', accountNum);
+
+      /*
+      for (int i = 0; i < data.length; i++) {
+        books.add(Book.fromJson(data[i]));
+      }
+      */
+      //return books;
+    } else {
+      throw Exception('Problem loading books');
+    }
+
+    /*
+    await prefs.setString('accountnum', 'accountnum value');
+    await prefs.setString('activeaccount', 'activeaccount value');
+    await prefs.setString('email', 'email value');
+    await prefs.setString('userid', 'userid value');
+    await prefs.setString('username', 'username value');
+    await prefs.setString('active_account_name', 'active_account_name value');
+    */
+  }
+
   @override
   bool operator ==(Object other) =>
       other is ProductstoreAuth && other._signedIn == _signedIn;
@@ -77,6 +120,7 @@ class ProductstoreAuth extends ChangeNotifier {
   @override
   int get hashCode => _signedIn.hashCode;
 }
+//End ProductstoreAuth
 
 class ProductstoreAuthScope extends InheritedNotifier<ProductstoreAuth> {
   const ProductstoreAuthScope({
