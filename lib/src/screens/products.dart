3ff5a439d60e4dda326496e500ca9858fb349030
joer80 <joer80@gmail.com>
2023-11-09 09:36:03 -0600

    search example

--- lib/src/screens/products.dart
+++ lib/src/screens/products.dart
@@ -6,7 +6,11 @@ import 'package:flutter/material.dart';
 
 import '../data.dart';
 import '../routing.dart';
-import '../widgets/product_search_delegate.dart';
+//import '../widgets/product_search_delegate.dart';
+
+import 'dart:async'; //optional but helps with debugging
+import 'dart:convert'; //to and from json
+import 'package:http/http.dart' as http; //for api requests
 
 class ProductsScreen extends StatefulWidget {
   const ProductsScreen({
@@ -17,38 +21,186 @@ class ProductsScreen extends StatefulWidget {
   State<ProductsScreen> createState() => _ProductsScreenState();
 }
 
-class _ProductsScreenState extends State<ProductsScreen>
-    with SingleTickerProviderStateMixin {
+class Debouncer {
+  int? milliseconds;
+  VoidCallback? action;
+  Timer? timer;
+
+  run(VoidCallback action) {
+    if (null != timer) {
+      timer!.cancel();
+    }
+    timer = Timer(
+      Duration(milliseconds: Duration.millisecondsPerSecond),
+      action,
+    );
+  }
+}
+
+class _ProductsScreenState extends State<ProductsScreen> {
+  final _debouncer = Debouncer();
+
+  List<Subject> ulist = [];
+  List<Subject> userLists = [];
+  //API call for All Subject List
+
+  String url = 'https://type.fit/api/quotes';
+
+  Future<List<Subject>> getAllulistList() async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        // print(response.body);
+        List<Subject> list = parseAgents(response.body);
+        return list;
+      } else {
+        throw Exception('Error');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  static List<Subject> parseAgents(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Subject>((json) => Subject.fromJson(json)).toList();
+  }
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Product Catalog'),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  showSearch(
-                      context: context,
-                      delegate: ProductSearchDelegate() //(products: products)
-                      );
-                },
-                icon: const Icon(Icons.search)),
-            IconButton(
-                icon: const Icon(Icons.shopping_cart),
-                onPressed: () {
-                  RouteStateScope.of(context).go('/cart');
-                }),
-          ], //for i
-          backgroundColor: Colors.green[700],
+  void initState() {
+    super.initState();
+    getAllulistList().then((subjectFromServer) {
+      setState(() {
+        ulist = subjectFromServer;
+        userLists = ulist;
+      });
+    });
+  }
+
+  //Main Widget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'All Users',
+          style: TextStyle(fontSize: 25),
         ),
-        body: Container(
-            alignment: Alignment.center,
-            child: Flex(
-                direction: Axis.vertical,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('search here',
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                ])),
-      );
+      ),
+      body: Column(
+        children: <Widget>[
+          //Search Bar to List of typed Subject
+          Container(
+            padding: EdgeInsets.all(15),
+            child: TextField(
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                suffixIcon: InkWell(
+                  child: Icon(Icons.search),
+                ),
+                contentPadding: EdgeInsets.all(15.0),
+                hintText: 'Search ',
+              ),
+              onChanged: (string) {
+                _debouncer.run(() {
+                  setState(() {
+                    userLists = ulist
+                        .where(
+                          (u) => (u.text.toLowerCase().contains(
+                                string.toLowerCase(),
+                              )),
+                        )
+                        .toList();
+                  });
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              padding: EdgeInsets.all(5),
+              itemCount: userLists.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(
+                            userLists[index].text,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          subtitle: Text(
+                            userLists[index].author ?? "null",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//Declare Subject class for json data or parameters of json string/data
+//Class For Subject
+/*
+class Subject {
+  var item_number;
+
+  Subject({
+    required this.item_number,
+  });
+
+  factory Subject.fromJson(Map<dynamic, dynamic> json) {
+    return Subject(
+      item_number: json['item_number'],
+    );
+  }
+}
+*/
+
+class Subject {
+  var text;
+  var author;
+  Subject({
+    required this.text,
+    required this.author,
+  });
+
+  factory Subject.fromJson(Map<dynamic, dynamic> json) {
+    return Subject(
+      text: json['text'],
+      author: json['author'],
+    );
+  }
 }
