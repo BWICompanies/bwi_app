8182ab0715d8086d9cd63bde6d99c245c28bfb4d
joer80 <joer80@gmail.com>
2023-11-09 08:20:49 -0600

    Add search basics to catalog

--- lib/src/screens/products.dart
+++ lib/src/screens/products.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 import '../data.dart';
 import '../routing.dart';
-import '../widgets/product_list.dart';
+//import '../widgets/product_list.dart';
 
 class ProductsScreen extends StatefulWidget {
   const ProductsScreen({
@@ -23,6 +23,21 @@ class _ProductsScreenState extends State<ProductsScreen>
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: const Text('Product Catalog'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showSearch(
+                      context: context,
+                      delegate: ProductSearchDelegate() //(products: products)
+                      );
+                },
+                icon: const Icon(Icons.search)),
+            IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                onPressed: () {
+                  RouteStateScope.of(context).go('/cart');
+                }),
+          ], //for i
           backgroundColor: Colors.green[700],
         ),
         body: Container(
@@ -36,4 +51,38 @@ class _ProductsScreenState extends State<ProductsScreen>
                           fontSize: 20, fontWeight: FontWeight.bold)),
                 ])),
       );
+}
+
+class ProductSearchDelegate extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Text('buildSuggestions');
+  }
 }
