c68b79b6ea1c56601dbc58dbf70101d684ff5b96
joer80 <joer80@gmail.com>
2023-11-16 17:29:31 -0600

    WIP

--- lib/src/screens/product_details.dart
+++ lib/src/screens/product_details.dart
@@ -17,51 +17,38 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   //build method for this class that takes in a BuildContext object and returns a Widget object. BuildContext is a handle to the location of a widget in the widget tree.
   Widget build(BuildContext context) {
-    //print(key);
-
-    if (item_number == null) {
-      return const Scaffold(
-        body: Center(
-          child: Text('No product found.'),
-        ),
-      );
-    }
     return Scaffold(
       appBar: AppBar(
         title: Row(children: [
           Text('Product: '),
-          Text("item_number here"),
+          Text(item_number ?? 'No item number'),
           //Text(product!.item_number),
         ]),
         backgroundColor: Colors.green[700],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(24.0), // You can adjust the padding as needed
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                "item_description here",
-                //product!.item_description,
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              SizedBox(height: 32.0),
-              Image.network(
-                //product!.image_urls[0],
-                "https://boss.bwicompanies.com/item/FS101",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 32.0),
-              Text(
-                //product!.item_description,
-                "item long description here",
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              SizedBox(height: 32.0),
-              Text(
-                  "add gridviewbuilder back here for uom stuff. Check search4"),
-            ],
-          ),
+      //Something in body is causing the error.
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "item_description here",
+              //product!.item_description,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            SizedBox(height: 32.0),
+            Text(
+              "image.network will go here.", //Maybe product!.image_urls[0]
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            SizedBox(height: 32.0),
+            Text(
+              //product!.item_description,
+              "item long description here",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(height: 32.0),
+            Text("add gridviewbuilder back here for uom stuff. Check search4"),
+          ],
         ),
       ),
     );
