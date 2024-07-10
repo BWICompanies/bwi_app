83e7a50174e713fed79c6cb7cdf9378c2f7131e7
joer80 <joer80@gmail.com>
2024-07-10 14:32:44 -0500

    Account WIP

--- lib/src/screens/account.dart
+++ lib/src/screens/account.dart
@@ -5,10 +5,32 @@ import '../routing.dart';
 import '../constants.dart'; //ie. var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
 import '../data.dart';
 
-class AccountScreen extends StatelessWidget {
-  final String title = 'Account';
-
+class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  final String title = 'Account';
+  String savedString = '';
+
+  //const AccountScreen({super.key});
+
+  @override
+  void initState() {
+    super.initState();
+    _getStringFromPrefs();
+  }
+
+  Future<void> _getStringFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      savedString = prefs.getString('token') ?? '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -21,25 +43,101 @@ class AccountScreen extends StatelessWidget {
           ),
         ),
         //body: Text('test'),
-        body: FutureBuilder(
-          future: readStringFromSharedPreferences(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Center(
-                child: Text(snapshot.data.toString()),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Account Information',
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold)),
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 7),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Account Number: ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: savedString,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 7),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Account Name: ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'A&W SHRUBS LLC',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Salesperson: ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'House Accounts',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
         ),
       );
 
+/*
   Future<String> readStringFromSharedPreferences() async {
     final prefs = await SharedPreferences.getInstance();
     final myString = prefs.getString('active_account_name');
     return myString ?? 'No data';
   }
+  */
 }
