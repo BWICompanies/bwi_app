9daa4280040e733f7be41958278efdb9b0f0ec8d
joer80 <joer80@gmail.com>
2024-08-06 09:31:31 -0500

    Add logo to sign in page

--- lib/src/screens/sign_in.dart
+++ lib/src/screens/sign_in.dart
@@ -52,11 +52,12 @@ class _SignInScreenState extends State<SignInScreen> {
           child: Card(
             child: Container(
               constraints: BoxConstraints.loose(const Size(600, 600)),
-              padding: const EdgeInsets.all(8),
+              padding: const EdgeInsets.all(25),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  Image.asset('assets/logo.png', height: 85),
                   Text('Sign in',
                       style: Theme.of(context).textTheme.headlineMedium),
                   TextField(
