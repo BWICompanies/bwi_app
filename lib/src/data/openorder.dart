97b2e55aa84478d3580147da0e19f2df334fc6a7
joer80 <joer80@gmail.com>
2024-10-03 15:19:59 -0500

    Made popups on card click. Reads shipping info. Working on reading lines

--- lib/src/data/openorder.dart
+++ lib/src/data/openorder.dart
@@ -32,7 +32,7 @@ class OpenOrder {
   var shiptoaddr2;
   var shiptocitystate;
   var shiptozip5;
-  //has lines sub array also.
+  final List<OrderLine> lines;
   var friendlyStatus;
 
   //Constructor
@@ -67,8 +67,28 @@ class OpenOrder {
     required this.shiptoaddr2,
     required this.shiptocitystate,
     required this.shiptozip5,
+    required this.lines,
     required this.friendlyStatus,
   });
+
+// Factory constructor for deserialization from JSON
+/*
+  factory OpenOrder.fromJson(Map<dynamic, dynamic> json) {
+    final data = json['data'] as List<dynamic>;
+    final openOrderData = data.first as Map<dynamic, dynamic>;
+
+    final orderNumber = openOrderData['order_number'] as String;
+    final lines = (openOrderData['lines'] as List<dynamic>)
+        .map(
+            (lineJson) => OrderLine.fromJson(lineJson as Map<dynamic, dynamic>))
+        .toList();
+
+    return OpenOrder(
+      orderNumber: orderNumber,
+      lines: lines,
+    );
+  }
+  */
 
   //factory method that accepts json map (associative array in PHP) and returns an ApiProduct object. (Uses the constructor above to create and return an instance of the obj class)
   factory OpenOrder.fromJson(Map<dynamic, dynamic> json) {
@@ -103,6 +123,7 @@ class OpenOrder {
       shiptoaddr2: json['shiptoaddr2'],
       shiptocitystate: json['shiptocitystate'],
       shiptozip5: json['shiptozip5'],
+      lines: [], //json['lines'].map<OrderLine>((json) => OrderLine.fromJson(json)).toList(),
       friendlyStatus: json['friendlyStatus'],
     );
   }
@@ -113,4 +134,45 @@ class OpenOrder {
     return OpenOrder(item_description: json['data']['item_description']);
   }
   */
+} //end class OpenOrder
+
+// Class representing individual order lines
+
+class OrderLine {
+  /*
+  final String orderNumber;
+  final String itemNumber;
+  final String itemDescription;
+  final String uom;
+  final String uomDesc;
+  final int quantity;
+  final double unitPrice;
+  final String upsEnabled;
+  final String packSize;
+  final String vendor;
+  final int qtyCancelled;
+  final int qtyMoved;
+
+  // Factory constructor for deserialization from nested JSON
+  factory OrderLine.fromJson(Map<dynamic, dynamic> json) {
+    return OrderLine(
+      orderNumber: json['ordernum'] as String,
+      itemNumber: json['item_number'] as String,
+      itemDescription: json['item_description'] as String,
+      uom: json['uom'] as String,
+      uomDesc: json['uom_desc'] as String,
+      quantity: int.tryParse(json['quantity'] as String) ??
+          0, // Handle potential parsing errors
+      unitPrice: double.tryParse(json['unitprice'] as String) ??
+          0.0, // Handle potential parsing errors
+      upsEnabled: json['ups_enabled'] as String,
+      packSize: json['pack_size'] as String,
+      vendor: json['vendor'] as String,
+      qtyCancelled: int.tryParse(json['qtycancelled'] as String) ??
+          0, // Handle potential parsing errors
+      qtyMoved: int.tryParse(json['qtymoved'] as String) ??
+          0, // Handle potential parsing errors
+    );
+    
+  }*/
 }
